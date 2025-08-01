48b09cf2d22ef24e527dd4121c922c0bbf40c2d0
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-08-01 13:56:30 +0530

    django day4

---
+++ day79/project2/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_no = models.CharField(max_length=20)
+    grade = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} (Roll No: {self.roll_no})"
